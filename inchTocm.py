e9741194be47d6980f8bc663e7ba7653f164c074
Xephilion <kimgeonhee2006@gmail.com>
2026-03-18 21:16:04 +0900

    Add files via upload

---
+++ inchTocm.py
@@ -0,0 +1,5 @@
+# 입력을 받습니다.
+input_inch = float(input("인치 입력> "))
+
+# 출력합니다.
+print("센티미터:", input_inch * 2.54)
---
+++ kgToPound.py
@@ -0,0 +1,2 @@
+input_kg = float(input("킬로그램 입력> "))
+print("파운드:", input_kg * 2.2046226)
